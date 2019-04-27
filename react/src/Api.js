955e7fe63c1f8007825bb241d250b04e95a4d079
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2019-04-27 05:35:26 -0700

    lint-staged and prettier configs

--- react/src/Api.js
+++ react/src/Api.js
@@ -1,176 +1,295 @@
+const getUsers = function(headerAuthorization, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/users', true);
+    xhr.setRequestHeader('Authorization', headerAuthorization);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
 
-var getUsers = function(headerAuthorization, onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/users', true);
-  xhr.setRequestHeader("Authorization", headerAuthorization);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(null);
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(null);
 };
 
-var getUsersById = function(id, headerAuthorization, onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/users/' + encodeURIComponent(id) + '', true);
-  xhr.setRequestHeader("Authorization", headerAuthorization);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(null);
+const getUsersById = function(id, headerAuthorization, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', String('/users/' + encodeURIComponent(id)), true);
+    xhr.setRequestHeader('Authorization', headerAuthorization);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(null);
 };
 
-var postUsers = function(body, headerAuthorization, onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/users', true);
-  xhr.setRequestHeader("Authorization", headerAuthorization);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(JSON.stringify(body));
+const postUsers = function(body, headerAuthorization, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/users', true);
+    xhr.setRequestHeader('Authorization', headerAuthorization);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(body));
 };
 
-var postEvents = function(body, headerAuthorization, onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/events', true);
-  xhr.setRequestHeader("Authorization", headerAuthorization);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(JSON.stringify(body));
+const postEvents = function(body, headerAuthorization, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/events', true);
+    xhr.setRequestHeader('Authorization', headerAuthorization);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(body));
 };
 
-var getMetrics = function(headerAuthorization, onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/metrics', true);
-  xhr.setRequestHeader("Authorization", headerAuthorization);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(null);
+const getMetrics = function(headerAuthorization, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/metrics', true);
+    xhr.setRequestHeader('Authorization', headerAuthorization);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(null);
 };
 
-var getEvents = function(onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/events', true);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(null);
+const getEvents = function(onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/events', true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(null);
 };
 
-var getEventsById = function(id, onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/events/' + encodeURIComponent(id) + '', true);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(null);
+const getEventsById = function(id, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', String('/events/' + encodeURIComponent(id)), true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(null);
 };
 
-var postLogin = function(body, onSuccess, onError) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/login', true);
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = function () {
-    var res = null;
-    if (xhr.readyState === 4) {
-      if (xhr.status === 204 || xhr.status === 205) {
-        onSuccess();
-      } else if (xhr.status >= 200 && xhr.status < 300) {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onSuccess(res);
-      } else {
-        try { res = JSON.parse(xhr.responseText); } catch (e) { onError(e); }
-        if (res) onError(res);
-      }
-    }
-  };
-  xhr.send(JSON.stringify(body));
+const postLogin = function(body, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/login', true);
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function() {
+        let res = null;
+        if (xhr.readyState === 4) {
+            if (xhr.status === 204 || xhr.status === 205) {
+                onSuccess();
+            } else if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onSuccess(res);
+                }
+            } else {
+                try {
+                    res = JSON.parse(xhr.responseText);
+                } catch (error) {
+                    onError(error);
+                }
+
+                if (res) {
+                    onError(res);
+                }
+            }
+        }
+    };
+
+    xhr.send(JSON.stringify(body));
 };
