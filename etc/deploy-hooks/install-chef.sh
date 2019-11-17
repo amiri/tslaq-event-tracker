fe86880654db0c74e159f132adf31c857db0158d
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2019-11-17 10:18:21 -0800

    Remove --force-yes

--- etc/deploy-hooks/install-chef.sh
+++ etc/deploy-hooks/install-chef.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DEBIAN_FRONTEND=noninteractive apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes -o Dpkg::Options::="--force-confold" -o DPkg::Options::="--force-confdef" dist-upgrade
-DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes -o Dpkg::Options::="--force-confold" -o DPkg::Options::="--force-confdef" chef
+DEBIAN_FRONTEND=noninteractive apt-get -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-confold" -o DPkg::Options::="--force-confdef" dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-confold" -o DPkg::Options::="--force-confdef" chef
