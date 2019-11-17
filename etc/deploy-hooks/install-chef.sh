097001000a068ec960e4c6a7cd2eaa0a97567b6e
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2019-11-17 10:50:16 -0800

    Add autoremove

--- etc/deploy-hooks/install-chef.sh
+++ etc/deploy-hooks/install-chef.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y autoremove
 DEBIAN_FRONTEND=noninteractive apt-get -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-confold" -o DPkg::Options::="--force-confdef" dist-upgrade
 DEBIAN_FRONTEND=noninteractive apt-get install -y --allow-unauthenticated --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-confold" -o DPkg::Options::="--force-confdef" chef
