cc86c8aba44820818301e4a84f32a1b754951261
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2019-11-27 22:41:20 -0800

    Switch order around in before-deploy

--- scripts/travis-before-deploy.sh
+++ scripts/travis-before-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-binary=$(find .stack-work/install/ -name "tslaq-event-tracker-exe")
 mkdir -p deploy/react
+binary=$(find .stack-work/install/ -name "tslaq-event-tracker-exe")
+cp $binary deploy
+cp react/dist/* deploy/react
 cd deploy
-cp $binary .
-cp ../react/dist/* react
 zip -r $TRAVIS_COMMIT .
 echo "deploy dir contents after zip:"
 ls -lastr
