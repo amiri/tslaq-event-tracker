589c5c7046a646bfe9135a23f086369930e32272
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2019-11-30 06:28:48 -0800

    Get basic_auth up, move map directives into custom nginx.conf template

--- etc/chef/cookbooks/nginx_wrapper/recipes/default.rb
+++ etc/chef/cookbooks/nginx_wrapper/recipes/default.rb
@@ -8,6 +8,8 @@ nginx_install 'default' do
     source 'repo'
     default_site_enabled false
     group 'tslaq'
+    cookbook 'nginx_wrapper'
+    template 'nginx.conf.erb'
     conf_variables(
         nginx_log_dir: '/var/local/tslaq-event-tracker/logs'
     )
