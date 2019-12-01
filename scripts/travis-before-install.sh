d7b0617cba9a6f81fb734170cf1aac6e9542bf39
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2019-12-01 07:05:33 -0800

    Remove new script, back to .travis.yml for before_install

--- scripts/travis-before-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-printenv
-mkdir -p ~/.local/bin
-export PATH=$HOME/.local/bin:$PATH
-travis_retry curl -L https://get.haskellstack.org/stable/linux-x86_64.tar.gz | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install --no-install-recommends yarn
