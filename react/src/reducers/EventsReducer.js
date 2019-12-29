f80b8d6e965028e0edcc0b6a3e18b97b2135c882
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2019-12-29 09:47:45 -0800

    Remove logging from event reducer

--- react/src/reducers/EventsReducer.js
+++ react/src/reducers/EventsReducer.js
@@ -1,6 +1,4 @@
 export const eventsReducer = (state, action) => {
-  console.log('eventsReducer state: ', state);
-  console.log('eventsReducer action: ', action);
   switch (action.type) {
     case 'GET_EVENTS': {
       return action.payload;
