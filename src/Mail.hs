211bc5d6b1cbd6d4e7e65e2eaf529998c49415d8
Amiri Barksdale at Home <amiribarksdale@gmail.com>
2020-02-26 10:26:37 -0800

    Remove mailgun packages and use wreq

--- src/Mail.hs
+++ src/Mail.hs
@@ -3,14 +3,15 @@
 module Mail where
 
 import           AppContext            (AppT (..), mailGunReplyTo)
+import           Control.Lens                ((&), (^?), (?~))
 import           Control.Monad.Except  (MonadIO, liftIO)
 import           Data.ByteString.Char8 (ByteString, pack)
 import           Data.Text             (Text, unpack)
-import           Errors
-import           Mail.Hailgun
-import           Servant               (throwError)
+-- import           Errors
+-- import           Servant               (throwError)
 import           Types
 import Debug.Trace
+import Network.Wreq
 
 email
   :: MonadIO m
@@ -26,25 +27,29 @@ email (MailGunDomain domain) (MailGunKey apiKey) subject message (UserEmail to)
     let recipient = pack (unpack to)
     traceM $ ("Hailgun replyTo: " ++ (show replyTo))
     traceM $ ("Hailgun recipient: " ++ (show recipient))
-    let context = HailgunContext (unpack domain) (unpack apiKey) Nothing
-    let msg = hailgunMessage
-          subject
-          (TextOnly message)
-          replyTo
-          (emptyMessageRecipients { recipientsTo = [recipient] })
-          []
-    traceM $ ("Hailgun message: " ++ (show msg))
-    case msg of
-      Left err -> do
-        traceM $ ("Hailgun creation error: " ++ (show err))
-        throwError $ encodeJSONError (JSONError 502 "EmailCreationError" err)
-      Right msg' -> do
-        result <- liftIO $ sendEmail context msg'
-        traceM $ ("Hailgun sending result: " ++ (show result))
-        case result of
-          Left err' -> do
-            traceM $ ("Hailgun error: " ++ (show err'))
-            traceM $ ("Hailgun error 2: " ++ (show (herMessage err')))
-            throwError $ encodeJSONError
-              (JSONError 502 "EmailSendError" (herMessage err'))
-          Right _ -> pure ()
+    let opts = defaults & auth ?~ basicAuth "api" (pack $ unpack apiKey)
+    res <- liftIO $ postWith opts (unpack domain) ["subject" := subject, "text" := message, "from" := replyTo, "to" := recipient]
+    traceM $ show (res ^? responseBody)
+
+    -- let context = HailgunContext (unpack domain) (unpack apiKey) Nothing
+    -- let msg = hailgunMessage
+    --       subject
+    --       (TextOnly message)
+    --       replyTo
+    --       (emptyMessageRecipients { recipientsTo = [recipient] })
+    --       []
+    -- traceM $ ("Hailgun message: " ++ (show msg))
+    -- case msg of
+    --   Left err -> do
+    --     traceM $ ("Hailgun creation error: " ++ (show err))
+    --     throwError $ encodeJSONError (JSONError 502 "EmailCreationError" err)
+    --   Right msg' -> do
+    --     result <- liftIO $ sendEmail context msg'
+    --     traceM $ ("Hailgun sending result: " ++ (show result))
+    --     case result of
+    --       Left err' -> do
+    --         traceM $ ("Hailgun error: " ++ (show err'))
+    --         traceM $ ("Hailgun error 2: " ++ (show (herMessage err')))
+    --         throwError $ encodeJSONError
+    --           (JSONError 502 "EmailSendError" (herMessage err'))
+    --       Right _ -> pure ()
